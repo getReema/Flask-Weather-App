08caaeccfdd8e49aabb5ec07f55550ee0e871ada
reema <reeroo02@gmail.com>
2020-07-13 18:52:55 +0300

    finished styling.

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ from flask import Flask, render_template, request
 app= Flask(__name__) #name is configured to be the name of the application during run time
 
 #second; map the app using routs
+#route for index page
 @app.route('/')
 def weather_dashboard():
     return render_template('home.html')
 
+#route for result page
 @app.route('/results', methods=['POST']) #the way to access this is via a POST Req
 def render_results():
     zip_code= request.form['zipCode'] #Accessing the html element named zipCode
@@ -20,17 +22,16 @@ def render_results():
     data= get_weather_results(zip_code,api_key)
     temp= "{0:.2f}".format(data["main"]["temp"]) #to get a value from the dictionary as string
     feels_like= "{0:.2f}".format(data["main"]["feels_like"])
-    weather= data["weather"][0]["main"]
+    weather= data["weather"][0]["main"] #array not a dictionary
     location= data["name"]
     return render_template('results.html', location=location,temp=temp,
                            feels_like=feels_like, weather=weather )
-
-
 
 def get_api_key():
     config=configparser.ConfigParser()
     config.read('config.ini')
     return config['openweathermap']['api']
+
 
 def get_weather_results(zip_code, api_key ):
     api_url= 'http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}'.format(zip_code, api_key)
@@ -39,9 +40,9 @@ def get_weather_results(zip_code, api_key ):
     #python will go ahead and request the data from the api url then
     #return it in json
 
-
+#just to check
 print(get_weather_results("95129", get_api_key()))
 
-
+#should be the last line
 if __name__== '__main__':
     app.run() #this ensures the app only run once and multiple instances are not crated
