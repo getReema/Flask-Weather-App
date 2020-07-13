20eed165b9625442be14df88feba539f1e650e9d
reema <reeroo02@gmail.com>
2020-07-13 15:35:19 +0300

    initiated the flask app

--- app.py
+++ app.py
@@ -2,6 +2,24 @@
 #to call our API we need the link URL
 import requests
 import configparser
+from flask import Flask
+
+#first; initilize the flask app
+app= Flask(__name__) #name is configured to be the name of the application during run time
+
+#second; map the app using routs
+@app.route('/')
+def weather_dashboard():
+    return "Hello world"
+
+@app.route('/results')
+def render_results():
+    return "Results Page"
+
+
+if __name__== '__main__':
+    app.run() #this ensures the app only run once and multiple instances are not crated
+
 
 def get_api_key():
     config=configparser.ConfigParser()
